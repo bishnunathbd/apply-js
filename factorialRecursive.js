a29dc5351ae07849999295db77cc182a265940f3
bishnunathbd <bishnunath774@gmail.com>
2020-07-21 12:06:46 +0600

    how recursion works

--- factorialRecursive.js
+++ factorialRecursive.js
@@ -18,6 +18,27 @@ function factorial(n) {
         return n * factorial(n - 1);
     }
 }     
+// var result = factorial(5);
+// console.log(result);
 
-var result = factorial(5);
-console.log(result);
+
+// 5! = 5*4*3*2*1
+
+// 5*factorialRecursive(4)
+// 5*4*factorialRecursive(3)
+// 5*4*3*factorialRecursive(2)
+// 5*4*3*2*factorialRecursive(1)
+// 5*4*3*2*1
+
+function factorialRecursive(num) {
+    if (num == 1) {
+        console.log(num);
+        return 1;
+    } else {
+        console.log(num, num - 1);
+        return num * factorialRecursive(num - 1);
+    }
+}
+var result = factorialRecursive(5);
+console.log('result', result);
+
